c0acc1db4c4d43019efda0da822db1e9b06506f2
Pablo Castro da Silva <91504069+silvapablo@users.noreply.github.com>
2025-07-12 02:02:45 -0400

    Fix Buffer Constructor Error in Vite and Main

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { Buffer } from 'buffer'
 
 export default defineConfig({
   plugins: [react()],
   define: {
     global: 'globalThis',
-    Buffer: 'globalThis.Buffer',
+    Buffer: ['buffer', 'Buffer'],
   },
   resolve: {
     alias: {
